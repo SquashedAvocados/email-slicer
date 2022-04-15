91b48f805fe89fd7c55aaf9fc950f7a52b0225d3
Avocado <38474028+SquashedAvocados@users.noreply.github.com>
2022-04-15 15:45:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 from VividHues import Clr
 
 
+print(Clr.rainbow("""\
+ _______  ___  ___       ___       __   __    
+|   ____||   \/   |     /   \     |  | |  |   
+|  |__   |  \  /  |    /  ^  \    |  | |  |   
+|   __|  |  |\/|  |   /  /_\  \   |  | |  |   
+|  |____ |  |  |  |  /  _____  \  |  | |  `--.
+|_______||__|  |__| /__/     \__\ |__| |_____|
+"""))
+print(Clr.rainbow("""\
+    ______  __     __    ____  _______  ______      
+   /      ||  |   |  |  /    ||   ____||   _  \     
+  |   (---`|  |   |  | |  ,--'|  |__   |  |_)  |    
+   \   \   |  |   |  | |  |   |   __|  |      /     
+.---)   |  |  `--.|  | |  `--.|  |____ |  |\  \--.
+|______/   |_____||__|  \____||_______||__| `.___|
+"""))
+
 input_email = input(f"{Clr.LIME}Please enter your email\n\t--> {Clr.RS}")
 input_email = input_email.strip()
 
@@ -8,6 +25,8 @@ while len(input_email) <= 6:  # small email example _@_.__
   Clr.delPrevLine(2)
   input_email = input(f"{Clr.LIME}Please enter your email\n\t--> {Clr.RS}")
   input_email = input_email.strip()
+
+Clr.delPrevLine(2)
 
 at_location = input_email.index("@")
 username    = input_email[:at_location]
@@ -17,3 +36,13 @@ print(f"{Clr.RED}Email:\t\t{input_email}")
 print(f"{Clr.PINK}Username:\t{username}")
 print(f"{Clr.BLUE}Domain:\t\t{' ' * len(username) + ' '}{domain}")
 print(Clr.RS, end="")
+
+box_line    = '•-' + '-' * 6 + 2 * ('-' * 4) + '-' * len(input_email) + '-•'
+#          "| "    + "Email:"+ 2 * "\t"      + len(email)             + " |"
+print(f"""
+{box_line}
+| {Clr.RED}Email:\t\t{input_email}{Clr.RS} |
+| {Clr.PINK}Username:\t\t{username}{Clr.RS}{' ' + ' ' * len(domain)} |
+| {Clr.BLUE}Domain:\t\t{' ' * len(username) + ' '}{domain}{Clr.RS} |
+{box_line}{Clr.RS}\
+""")
